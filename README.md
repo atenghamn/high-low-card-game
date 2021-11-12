1f2037712da0d94446bd1635f4697073beddb7e6
Alexander Tenghamn <alex.tenghamn@gmail.com>
2021-11-12 16:50:21 +0100

    gamelogic, first commit

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ https://deckofcardsapi.com/
 
 ### Image license 
 https://unsplash.com/license
+
+### TODOS
+[] Write a switch to decide wich if a point is give
+[x] Create a global variabel for points 
+[] Fix "Draw card" button 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let deck = {};
+let points = 0;
 
 async function getCards() {
     // En asynkron funktion osm vi anropar fråt root för att hämta vårat sdeck så fort vår kod laddas och exkveras 
@@ -12,7 +13,7 @@ async function getCards() {
 }
 
 getCards(); // Anropar funktionen 
-drawFirstCard();
+
 
 
 const drawCardButton = document.getElementById("drawCard"); // Link our HTMLbutton 
@@ -25,6 +26,8 @@ drawCardButton.addEventListener("click", async () => {
 })
 
 const cardPlaceholder = document.getElementById("cardPlaceholder")
+const pointsNumber = document.getElementById("point");
+pointsNumber.innerText = points;
 
 function printCard(cards) {
     const cardTitle = document.getElementById("cardTitle");
@@ -50,4 +53,230 @@ higherButton.addEventListener("click", async () => {
     const data = await res.json();
     console.log(data.cards[0]); // Logging the first card
     printCard(data.cards[0]);
+    checkValue(1, data.cards[0].value)
 })
+
+function checkValue(numb, card) {
+    console.log(card)
+    if (numb === 1){
+
+        switch(card) {
+            case "2": 
+            parseInt(card);
+            if (numb >= card) {
+                points++;
+            }
+            break;
+
+            case "3": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "4": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "5": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "6": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "7": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "8": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "9": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "10": 
+            parseInt(card);
+            if (numb > card) {
+                points++;
+            }
+            break;
+            
+            case "KNIGHT": 
+            card = 11;
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "QUEEN":
+            card = 12; 
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "KING":
+            card = 13; 
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "ACE":
+            card = 14; 
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+            case "JACK":  
+            card = 15;
+            if (numb > card) {
+                points++;
+            }
+            break;
+
+
+            default:
+                console.log("Fuck...something went wrong! ");
+                break;
+    
+        } 
+
+    }
+    
+        else if(numb === 2) {
+    
+        switch(card) {
+            case "2": 
+            parseInt(card);
+            if (numb <= card) {
+                points++;
+            }
+            break;
+
+            case "3": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "4": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "5": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "6": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "7": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "8": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "9": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "10": 
+            parseInt(card);
+            if (numb < card) {
+                points++;
+            }
+            break;
+            
+            case "KNIGHT": 
+            card = 11;
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "QUEEN":
+            card = 12; 
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "KING":
+            card = 13; 
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "ACE":
+            card = 14; 
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            case "JACK":  
+            card = 15;
+            if (numb < card) {
+                points++;
+            }
+            break;
+
+            default:
+                console.log("Fuck...something went wrong! ");
+                break;
+        
+         
+
+
+        }
+    }
+}
+    
